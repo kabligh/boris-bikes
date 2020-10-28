28bef6c90daf4fb3c80ef5a3ff7c17aea2557813
K Annabel Bligh <kabligh@gmail.com>
2020-10-28 11:28:41 +0000

    spec folder with docking_station_spec

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,8 @@
+require 'docking_station'
+
+describe DockingStation do
+  it { is_expected.to respond_to :release_bike }
+end
+
+# it "responds to release_bike" do
+  # expect(subject).to respond_to :release_bike
