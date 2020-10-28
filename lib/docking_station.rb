911e59ae942184ebb1fb6ca17ec5fc23880a22eb
Alex Leeson-Mill <alexleesonmill@gmail.com>
2020-10-28 16:53:40 +0000

    passing exercise 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,10 +1,11 @@
 class DockingStation
-attr_reader :bike
+  attr_reader :bike
 
   def release_bike
     @bike = Bike.new
   end
-  def dock_bike(bike)
+
+  def dock(bike)
     @bike = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,15 +5,15 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
   it { expect(DockingStation.new.release_bike).is_a?(Bike) }
   it { expect(Bike.new.working?).to eq(true) }
-  it { is_expected.to respond_to :dock_bike }
-  it "checks if dock_bike has docked a bike" do
+  it { is_expected.to respond_to :dock }
+  it 'checks if dock_bike has docked a bike' do
     bike = subject.release_bike
-    expect(bike.dock_bike(bike)).is_a?(Bike)
+    expect(subject.dock(bike)).is_a?(Bike)
   end
 end
 
 # it "responds to release_bike" do
-  # expect(subject).to respond_to :release_bike
+# expect(subject).to respond_to :release_bike
 
 # it "release_bike returns a bike" do
 #  expect(DockingStation.new.release_bike).is_a?(Bike)
