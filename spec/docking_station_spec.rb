d2ac4cbbcdeae4e9b1abc1ae3408fe407fc6b6e6
K Annabel Bligh <kabligh@gmail.com>
2020-10-29 17:28:21 +0000

    Chapter 12 – raising exceptions

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,30 +1,38 @@
 require 'docking_station'
+require_relative 'bike'
 
 describe DockingStation do
-  it { is_expected.to respond_to(:release_bike) }
 
-  it "releases working bikes" do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe '#release_bike' do
+
+    it { is_expected.to respond_to(:release_bike) }
+
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      # we want to release the bike that is docked
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'raises an error when no bikes available' do
+      expect { subject.release_bike }.to raise_error "No bikes available"
+    end
+
   end
 
-  it "docks a bike" do
-    bike = Bike.new
-    # We want to return the bike we dock
-    expect(subject.dock(bike)).to eq bike
+  describe '#working?' do
+    it 'releases working bikes' do
+      subject.dock(Bike.new)
+      bike = subject.release_bike
+      expect(bike).to respond_to(:working?)
+    end
   end
 
-  it "returns docked bikes" do
-    bike = Bike.new
-    subject.dock(bike)
-    # return the bike we just docked
-    expect(subject.bike).to eq bike
+  describe '#dock' do
+    it 'docks bike at docking station' do
+      bike = Bike.new
+      station = DockingStation.new
+      expect(station.dock(bike)).to eq bike
+    end
   end
-
 end
-
-# it "responds to release_bike" do
-# expect(subject).to respond_to :release_bike
-
-# { is_expected.to respond_to(:dock).with(1).argument }
-# { is_expected.to respond_to(:bike)}
