75809795851998d6740304c254a0e3ebe36d90a4
K Annabel Bligh <kabligh@gmail.com>
2020-11-01 17:37:14 +0000

    Challenge 15 – private methods for full and empty

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,20 +1,30 @@
 require_relative 'bike'
 
 class DockingStation
-#  attr_reader :bikes
+  attr_reader :bikes
 
   def initialize
     @bikes = []
   end
 
   def release_bike
-    fail "No bikes available" if @bikes.empty?
+    fail "No bikes available" if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail "Docking station full" if @bikes.count >= 20
+    fail "Docking station full" if full?
     @bikes << bike
+  end
+
+  private
+
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty?
   end
 
 end
